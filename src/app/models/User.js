f255c61d81aa25b81d69c824d414c588ab6e2881
David Ivan Hinsching <david.ivan@magnus.com.br>
2020-03-06 07:51:54 -0300

    feat: user controller and password hash
    implemented user create on controller and password hash using bcrypt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
 
 class User extends Model {
     static init(sequelize) {
@@ -6,11 +7,20 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN
             },
             { sequelize }
         )
+
+        this.addHook('beforeSave', user => {
+            if (user.password) {
+                user.password_hash = bcrypt.hashSync(user.password, 10)
+            }
+        })
+
+        return this
     }
 }
 
